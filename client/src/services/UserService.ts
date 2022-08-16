193f6352b90d8cc269276dd1d0fd5d25f9f4d026
Monayam Reshad <reshadmrrr@gmail.com>
2022-08-16 13:30:33 +0600

    added user service

---
+++ client/src/services/UserService.ts
@@ -0,0 +1,32 @@
+export class UserService {
+
+    public async getAllUsers(): Promise<any> {
+        const response = await fetch('api/users');
+        return await response.json();
+    }
+
+    public async createUser(user: any): Promise<any> {
+        const response = await fetch('api/user', { 
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(user),
+         });
+         return await response.json();
+    }
+
+    public async updateUser(user: any): Promise<any> {
+        const response = await fetch('api/user', { 
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(user),
+         });
+        return await response.json();
+    }
+
+    public async deleteUser(userId: number): Promise<any> {
+        const response = await fetch(`api/user/${userId}`, {
+            method: 'DELETE',            
+        });
+        return await response.json();
+    }
+}
